860901ec9101bec5715a2728cb36f2fb0d07acf2
Kaushik Kumar Barman <kaushik.kb4@gmail.com>
2021-11-25 03:12:58 +0530

    Initialized markup and design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="calculator-grid">
+        <div className="output">
+          <div className="previous-output">45 +</div>
+          <div className="current-output">69</div>
+        </div>
+        <div className="calc-button calc-large-button">AC</div>
+        <div className="calc-button">DEL</div>
+        <div className="calc-button">/</div>
+        <div className="calc-button">1</div>
+        <div className="calc-button">2</div>
+        <div className="calc-button">3</div>
+        <div className="calc-button">*</div>
+        <div className="calc-button">4</div>
+        <div className="calc-button">5</div>
+        <div className="calc-button">6</div>
+        <div className="calc-button">+</div>
+        <div className="calc-button">7</div>
+        <div className="calc-button">8</div>
+        <div className="calc-button">9</div>
+        <div className="calc-button">-</div>
+        <div className="calc-button">.</div>
+        <div className="calc-button">0</div>
+        <div className="calc-button calc-large-button">=</div>
+      </div>
     </div>
   );
 }
